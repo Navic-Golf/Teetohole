f0508cb315ef73503fbe7676ed2c3ae855eec164
Oscar Dahlström <oscard0808@outlook.com>
2025-06-25 17:33:38 +0200

    Fix env vars inside GET function

--- src/app/api/get-feedback/route.ts
+++ src/app/api/get-feedback/route.ts
@@ -1,11 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_KEY!
-);
-
 export async function GET() {
+  const supabase = createClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_KEY!
+  );
+
   const { data, error } = await supabase.from('feedback').select('*');
 
   return new Response(JSON.stringify({ data, error }), {
